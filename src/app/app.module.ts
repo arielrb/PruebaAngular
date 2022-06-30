470cda435ed9f59601510a4505213964a2db0885
Ariel <restagno31@gmail.com>
2022-06-30 15:30:02 -0300

    Item de tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonComponent } from './boton/boton.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonComponent
+    BotonComponent,
+    TasksComponent,
+    TasksItemComponent
   ],
   imports: [
     BrowserModule
